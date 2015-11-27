9fa20543410a88d1ff91327c0a5b5c082a5c9429
Andrés Gomez <agomez@redborder.net>
2015-11-27 09:52:23 +0100

    TryCatch fileNotFoundException.

--- indexing-hadoop/src/main/java/io/druid/indexer/hadoop/FSSpideringIterator.java
+++ indexing-hadoop/src/main/java/io/druid/indexer/hadoop/FSSpideringIterator.java
@@ -17,83 +17,80 @@
 
 package io.druid.indexer.hadoop;
 
+import com.metamx.common.logger.Logger;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
  */
-public class FSSpideringIterator implements Iterator<FileStatus>
-{
-  public static FSSpideringIterator spiderPathPropogateExceptions(FileSystem fs, Path path)
-  {
-    try {
-      final FileStatus[] statii = fs.listStatus(path);
-      return new FSSpideringIterator(fs, statii == null ? new FileStatus[]{} : statii);
-    }
-    catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
+public class FSSpideringIterator implements Iterator<FileStatus> {
+    private static final Logger log = new Logger(FSSpideringIterator.class);
 
-  public static Iterable<FileStatus> spiderIterable(final FileSystem fs, final Path path)
-  {
-    return new Iterable<FileStatus>()
-    {
-      public Iterator<FileStatus> iterator()
-      {
-        return spiderPathPropogateExceptions(fs, path);
-      }
-    };
-  }
-
-  private final FileSystem fs;
-  private final FileStatus[] statii;
-
-  FSSpideringIterator statuses = null;
-  int index = 0;
-
-  public FSSpideringIterator(
-      FileSystem fs,
-      FileStatus[] statii
-  )
-  {
-    this.fs = fs;
-    this.statii = statii;
-  }
-
-  public boolean hasNext()
-  {
-    if (statuses != null && !statuses.hasNext()) {
-      statuses = null;
-      index++;
-    }
-    return index < statii.length;
-  }
-
-  public FileStatus next()
-  {
-    while (hasNext()) {
-      if (statii[index].isDir()) {
-        if (statuses == null) {
-          statuses = spiderPathPropogateExceptions(fs, statii[index].getPath());
-        } else if (statuses.hasNext()) {
-          return statuses.next();
+    public static FSSpideringIterator spiderPathPropogateExceptions(FileSystem fs, Path path) {
+        try {
+            final FileStatus[] statii = fs.listStatus(path);
+            return new FSSpideringIterator(fs, statii == null ? new FileStatus[]{} : statii);
+        } catch (FileNotFoundException e) {
+            log.info("File {} doesn't exist. Continue ...", path);
+            return new FSSpideringIterator(fs, new FileStatus[]{});
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-      } else {
-        ++index;
-        return statii[index - 1];
-      }
     }
-    throw new NoSuchElementException();
-  }
 
-  public void remove()
-  {
-    throw new UnsupportedOperationException();
-  }
+    public static Iterable<FileStatus> spiderIterable(final FileSystem fs, final Path path) {
+        return new Iterable<FileStatus>() {
+            public Iterator<FileStatus> iterator() {
+                return spiderPathPropogateExceptions(fs, path);
+            }
+        };
+    }
+
+    private final FileSystem fs;
+    private final FileStatus[] statii;
+
+    FSSpideringIterator statuses = null;
+    int index = 0;
+
+    public FSSpideringIterator(
+            FileSystem fs,
+            FileStatus[] statii
+    ) {
+        this.fs = fs;
+        this.statii = statii;
+    }
+
+    public boolean hasNext() {
+        if (statuses != null && !statuses.hasNext()) {
+            statuses = null;
+            index++;
+        }
+        return index < statii.length;
+    }
+
+    public FileStatus next() {
+        while (hasNext()) {
+            if (statii[index].isDir()) {
+                if (statuses == null) {
+                    statuses = spiderPathPropogateExceptions(fs, statii[index].getPath());
+                } else if (statuses.hasNext()) {
+                    return statuses.next();
+                }
+            } else {
+                ++index;
+                return statii[index - 1];
+            }
+        }
+        throw new NoSuchElementException();
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
 }
